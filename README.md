cb0d4613a94b42e2f85c9f9b94daf466769325d9
Catalin Sandu <catalin.sandu@windriver.com>
2022-04-18 14:18:12 +0300

    Minor fixes - add support for running the install-java.sh as root
    Signed-off-by: Catalin Sandu <catalin.octavian95@gmail.com>

--- README.md
+++ README.md
@@ -4,7 +4,10 @@ Install Java 1.7 on Ubuntu 20.04
 # Run
 sudo ./install-java.sh
 
-# Configure alternatives
+# Check Java version:
+java -version
+
+# Configure alternatives for JAVA
 1. sudo update-alternatives --config java
 2. sudo update-alternatives --config javac
 3. sudo update-alternatives --config javap
--- install-java.sh
+++ install-java.sh
@@ -3,7 +3,16 @@
 # Author: Catalin Sandu
 
 
-echo -e "> Java 1.7 installation ..."
+echo "***********************************"
+echo -e "Start Java 1.7 installation ..."
+echo "***********************************"
+
+# Make sure the script is running as root
+if [ "$(id -u)" != "0" ]; then
+    echo "You must be root to run $0. Try following:"
+    echo "sudo $0"
+    exit 1
+fi
 
 sudo dpkg -i lib/libjpeg62-turbo_1.5.2-2+b1_amd64.deb
 sudo dpkg -i lib/libsctp1_1.0.18+dfsg-1_amd64.deb
